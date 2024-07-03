98c3040552f2538a1206585d23a4c968abae1cd8
Sup#2.0 <sup2point0@gmail.com>
2024-07-03 21:22:45 +0100

    assort

---
+++ .site/src/parts/core/Article.svelte
@@ -0,0 +1,5 @@
+<main>
+  <article>
+    The quick brown fox jumps over the lazy dog
+  </article>
+</main>
---
+++ .site/src/routes/(article)/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+
+// import "#styles/essence.scss";
+
+import Nav from "#parts/core/Nav.svelte";
+import Article from "#parts/core/Article.svelte";
+import Footer from "#parts/core/Footer.svelte";
+
+</script>
+
+
+<div class="duality">
+  <div id="page">
+    <div id="nav-page-split">
+      <Nav />
+      <Article />
+    </div>
+    <Footer />
+  </div>
+</div>
+
+
+<style lang="scss">
+
+#nav-page-split {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: start;
+}
+
+</style>
