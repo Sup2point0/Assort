1c92a2cd2e300f0195f96679ec6ceaf4e661b8b4
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-04 07:35:46 +0000

    assort

--- .site/src/modules/stores/nav.ts
+++ .site/src/modules/stores/nav.ts
@@ -1,5 +1,16 @@
 import { persisted } from "svelte-persisted-store";
 
 
-const navOpen = persisted("assort-nav-state", true);
-export default navOpen;
+export class NavData {
+  open: boolean = true;
+  sections: object = {
+    Categories: true,
+    Creations: true,
+    Franchises: true,
+    Meta: true,
+  };
+}
+
+
+const nav = persisted("assort-nav", new NavData());
+export default nav;
