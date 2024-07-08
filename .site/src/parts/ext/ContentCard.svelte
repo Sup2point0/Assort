5bb952bb752870d57d7f78762756be42ed3a0215
Sup#2.0 <sup2point0@gmail.com>
2024-07-08 11:37:47 +0100

    assort

---
+++ .site/src/parts/ext/ContentCard.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+
+import { base } from "$app/paths";
+
+export let title: string;
+  export let col: string | undefined = undefined;
+export let capt: string | undefined = undefined;
+export let text: string = "";
+export let link: string | null = null;
+  export let intern: string | null = null;
+  export let extern: string | null = null;
+
+export let width: number | undefined = undefined;
+export let right: boolean = false;
+
+</script>
+
+
+{#if extern}
+  <a
+    class="no-link-style"
+    class:right
+    href={link}
+    target="_blank"
+    style:--width="{width}rem"
+  >
+    <h4 style:--col={col}> {title} </h4>
+    {#if capt}
+      <p class="caption"> {capt }</p>
+    {/if}
+    <p> {text} </p>
+  </a>
+
+{:else}
+  <a
+    class="no-link-style"
+    class:right
+    href="{intern ? base + "/" : ""}{intern ?? link}"
+    style:--width="{width}rem"
+  >
+    <h4 style:--col={col}> {title} </h4>
+    <p> {text} </p>
+  </a>
+
+{/if}
+
+
+<style lang="scss">
+
+a {
+  width: var(--width, auto);
+  min-width: 4rem;
+  margin: 0.25rem;
+  padding: 0.5rem 1rem;
+  display: inline-block;
+
+  text-align: left;
+  &.right { text-align: right; }
+  background-color: $col-back;
+
+  @include trans-default;
+
+  &:hover {
+    background-color: $col-back-deut;
+  }
+
+  @include focus-outline;
+}
+
+h4 {
+  @include font-ui;
+  font-size: 125%;
+  color: var(--col, $col-link);
+}
+
+p {
+  @include font-body;
+  font-size: 100%;
+}
+
+</style>
