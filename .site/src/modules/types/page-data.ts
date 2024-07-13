5461993711938795f143284e30fa5db8ad9a5b43
Sup#2.0 <sup2point0@gmail.com>
2024-07-13 15:53:59 +0100

    assort

---
+++ .site/src/modules/types/page-data.ts
@@ -0,0 +1,14 @@
+export default interface PageData {
+  path: string;
+  isIndex: boolean;
+  isFeatured: boolean;
+  isWoozy: boolean;
+
+  title: string;
+  head: string;
+  capt: string;
+  desc: string;
+  index: string[];
+  shard: string[];
+  dateDisplay: string;
+}
