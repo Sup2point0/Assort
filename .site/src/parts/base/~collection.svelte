badd3e2626fb33b149de864a4dd350691fdc39ba
Sup#2.0 <sup2point0@gmail.com>
2024-07-13 15:42:19 +0100

    assort

---
+++ .site/src/parts/base/~collection.svelte
@@ -0,0 +1,35 @@
+<!-- Squarkdown base for shard collection pages -->
+
+<script>
+
+import site from "#src/site-config";
+
+import Header from "#parts/page/Header.svelte";
+import Search from "#parts/ext/Search.svelte";
+import LinkCard from "#parts/ext/LinkCard.svelte";
+
+import { search } from "#modules/stores";
+
+
+const shard = "%{shard}";
+const collection = site.collections[shard];
+
+const page = {
+  title: collection.title + " • Collections"
+};
+
+const store = $search[shard];
+$: filteredPages = store.filtrate(collection.pages);
+
+</script>
+
+
+<Header {page} />
+
+<Search />
+
+<ul>
+  {#each filteredPages as page}
+    <LinkCard {...page} />
+  {/each}
+</ul>
