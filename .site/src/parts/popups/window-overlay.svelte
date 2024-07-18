a28336c1c93cbeb141a477879c5d8a37c6a35052
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-18 12:20:27 +0000

    assort

---
+++ .site/src/parts/popups/window-overlay.svelte
@@ -0,0 +1,35 @@
+<!-- @component WindowOverlay
+A blurred overlay across the entire screen for displaying modals or warnings over.
+-->
+
+<script lang="ts">
+
+import type { ClosableState } from "#modules/types";
+
+export let state: ClosableState;
+
+</script>
+
+<div class="overlay" on:click={$state.close}>
+  <slot>
+    <p> Uh, something went wrong! Click anywhere to exit... </p>
+  </slot>
+</div>
+
+
+<style lang="scss">
+
+.overlay-back {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  background-color: rgba(var(--col-back), 69%);
+  backdrop-filter: blur(8px);
+}
+
+</style>
