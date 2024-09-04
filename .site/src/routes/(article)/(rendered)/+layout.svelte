5d4ebefb99c9aad63aed74643b183be6b301f168
Sup#2.0 <sup2point0@gmail.com>
2024-09-04 09:16:54 +0100

    assort

---
+++ .site/src/routes/(article)/(rendered)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+
+import IndexView from "#parts/page/view.index.svelte";
+import PageFooter from "#parts/page/footer.page.svelte";
+
+</script>
+
+<slot />
+
+<IndexView />
+<!-- <PageFooter /> -->
--- .site/src/routes/(article)/+layout.svelte
+++ .site/src/routes/(article)/+layout.svelte
@@ -8,8 +8,6 @@ import Tools from "#parts/core/tools.svelte";
 import Footer from "#parts/core/footer.svelte";
 
 import Header from "#parts/page/header.page.svelte";
-import IndexView from "#parts/page/view.index.svelte";
-import PageFooter from "#parts/page/footer.page.svelte";
 
 import WindowOverlay from "#parts/popups/window-overlay.svelte";
 import Preferences from "#parts/popups/prefs/preferences.svelte";
@@ -27,9 +25,6 @@ import Preferences from "#parts/popups/prefs/preferences.svelte";
       <slot>
         <p class="error"> Uh, something has gone catastrophically, catastropically wrong! </p>
       </slot>
-
-      <IndexView />
-      <!-- <PageFooter /> -->
     </main>
 
     <Tools />
