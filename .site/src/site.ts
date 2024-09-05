1c3373a79e2bd9aea328939ee68764b20731e20c
Sup#2.0 <sup2point0@gmail.com>
2024-09-05 20:42:39 +0100

    assort

--- .site/src/site.ts
+++ .site/src/site.ts
@@ -12,38 +12,32 @@ const Site = {
   index:
   {
     dev: {
-      display: "dev",
-      colour: ["9090f1"],
-      pages: data.index.dev,
+      display: "dev", colour: ["9090f1"],
+      pages: data.index.dev
     },
     writing: {
-      display: "writing",
-      colour: ["4090f1"],
-      pages: data.index.writing,
+      display: "writing", colour: ["4090f1"],
+      pages: data.index.writing
     },
     poetry: {
-      display: "poetry",
-      colour: ["f190f1"],
-      pages: data.index.poetry,
+      display: "poetry", colour: ["f190f1"],
+      pages: data.index.poetry
     },
     yugioh: {
-      display: "Yu-Gi-Oh!",
-      colour: ["4090f1"],
-      pages: data.index.yugioh,
+      display: "Yu-Gi-Oh!", colour: ["4090f1"],
+      pages: data.index.yugioh
     },
     "yugioh-archetypes": {
-      display: "Yu-Gi-Oh! Archetypes",
-      colour: ["ff0090"],
-      pages: data.index["yugioh-archetypes"],
+      display: "Yu-Gi-Oh! Archetypes", colour: ["ff0090"],
+      pages: data.index["yugioh-archetypes"]
     },
   },
 
   shard:
   {
     sad: {
-      display: "sad",
-      colour: "#a9b0b8",
-    }
+      display: "sad", colour: "#a9b0b8"
+    },
   },
 };
 export default Site;
