1430f975748855286b82bdfbdfad9b6ee241b903
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-09-12 09:43:24 +0100

    assort

---
+++ .site/src/modules/utils/search-ratio.ts
@@ -0,0 +1,25 @@
+import { ratio as fuzzyRatio } from "fuzzball";
+
+
+export default function searchRatio(
+  source: string,
+  query: string,
+): number
+{
+  let sources = source.split(" ");
+  let queries = query.split(" ");
+
+  let ratio: number;
+  let apexRatio = 0;
+
+  for (let query in queries) {
+    for (let part in sources) {
+      ratio = fuzzyRatio(query, part);
+      if (ratio > apexRatio) {
+        apexRatio = ratio;
+      }
+    }
+  }
+
+  return ratio;
+}
