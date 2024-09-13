899e0b10588fc40ba340e46d6222409419efa65a
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-09-13 09:56:23 +0100

    assort

---
+++ .site/src/routes/(article)/(static)/featured/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+
+import Site from "#src/site";
+
+import type { PageData } from "#modules/types";
+import SearchOptions from "#modules/types";
+
+import Search from "#parts/ext/Search.svelte";
+
+
+const featured = Site.featured;
+let searchOptions = new SearchOptions();
+
+$: let featuredPages = searchOptions.apply(featured);
+
+</script>
+
+
+<Search bind:options={searchOptions} />
+
+<ul>
+  {#each featuredPages as page}
+    <ContentCard
+      title={page.title}
+      capt={page.capt}
+      desc={page.desc}
+    />
+  {/each}
+</ul>
+
+
+<style lang="scss">
+
+ul {
+  display: grid;
+}
+
+</style>
