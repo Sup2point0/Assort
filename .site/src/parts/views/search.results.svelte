2307c6bc2c111efc80e6b10dc7db6469d5fff2e7
Sup#2.0 <sup2point0@gmail.com>
2024-09-21 21:25:28 +0100

    assort

---
+++ .site/src/parts/views/search.results.svelte
@@ -0,0 +1,40 @@
+<!-- @component SearchResults
+
+-->
+
+<script lang="ts">
+
+import { SearchOptions } from "#modules/types";
+import type { PageData } from "#modules/types";
+
+import ContentCard from "#parts/ext/card.content.svelte";
+
+export let pages: PageData[];
+export let options: SearchOptions<PageData>;
+
+
+</script>
+
+
+<ul>
+  {#each options.apply(pages) as page}
+    <ContentCard
+      title={page.title ?? "..."}
+      capt={page.capt ?? "..."}
+      intern={page.dest}
+    >
+      <p> {page.desc} </p>
+    </ContentCard>
+  {/each}
+</ul>
+
+
+<style lang="scss">
+
+ul {
+  margin-top: 2rem;
+  padding: 0;
+  display: grid;
+}
+
+</style>
--- .site/src/routes/(article)/(static)/search/+page.svelte
+++ .site/src/routes/(article)/(static)/search/+page.svelte
@@ -6,12 +6,11 @@ import { SearchOptions } from "#modules/types";
 import type { PageData } from "#modules/types";
 
 import Search from "#parts/ext/search.svelte";
-import ContentCard from "#parts/ext/card.content.svelte";
+import SearchResults from "#parts/views/search.results.svelte";
 
 import { page } from "$app/stores";
 
 
-const pages = Object.values(Site.pages);
 const params = $page.url.searchParams;
 
 let searchOptions = new SearchOptions<PageData>({
@@ -24,25 +23,13 @@ let searchOptions = new SearchOptions<PageData>({
 
 <Search bind:options={searchOptions} />
 
-<ul>
-  {#each searchOptions.apply(pages) as page}
-    <ContentCard
-      title={page.title ?? "..."}
-      capt={page.capt ?? "..."}
-      intern={page.dest}
-    >
-      <p> {page.desc} </p>
-    </ContentCard>
-  {/each}
-</ul>
+<SearchResults pages={Object.values(Site.pages)}
+  options={searchOptions}
+/>
 
 
 <style lang="scss">
 
-ul {
-  margin-top: 2rem;
-  padding: 0;
-  display: grid;
-}
+
 
 </style>
