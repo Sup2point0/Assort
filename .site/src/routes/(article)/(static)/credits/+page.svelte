e1d1c4cdd5a9e3f1c1e3bf308bc40935c8380bb3
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-09-30 08:15:17 +0100

    assort

---
+++ .site/src/routes/(article)/(static)/credits/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+
+<h2> Fonts </h2>
+
+<ContentCard title="Fira Sans"
+  capt="Google Fonts"
+  link="https://fonts.google.com/specimen/Fira+Sans"
+/>
+
+
+<style lang="scss">
+
+
+
+</style>
