d0e92050aa09241dcdeb6a8a424c2e0d70091411
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-07 14:56:07 +0000

    assort

---
+++ .site/src/routes/notices.corner.svelte
@@ -0,0 +1,41 @@
+<!-- @component CornerNotices -->
+
+<script>
+
+import { popups } from "#modules/stores";
+
+import Notice from "#parts/popups/notice.corner.svelte";
+
+</script>
+
+
+<div class="notices">
+
+{#if $popups.data == false}
+  <Notice store:bind={$popups.data}
+    title="Welcome to Assort!"
+  >
+    This site doesn’t use cookies or analytics of any kind, but it does store your preferences in `localStorage`. See <a href="{base}/privacy">Privacy</a> for more. Enjoy <em>Assort</em>!
+  </Notice>
+{/if}
+
+{#if $popups.data == true && $popups.dev == false}
+  <Notice store:bind={$popups.dev}
+    title="Under Development"
+  >
+    ...
+  </Notice>
+{/if}
+
+</div>
+
+
+<style lang="scss">
+
+.notices {
+  position: fixed;
+  right: 1rem;
+  bottom: 1rem;
+}
+
+</style>
