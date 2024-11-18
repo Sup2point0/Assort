fa20517de9be9a9918bdb8053f935ac1cff16bf2
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-18 11:21:20 +0000

    assort

---
+++ .site/src/routes/(article)/(static)/stats/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+import Site from "#src/site";
+
+</script>
+
+
+<table>
+  <tr>
+    <th> source pages </th>
+    <td> ? </td>
+  </tr>
+  <tr>
+    <th> deployed pages </th>
+    <td> Site.pages.length </td>
+  </tr>
+  <tr>
+    <th> total slocs </th>
+    <td> ? </td>
+  </tr>
+  <tr>
+    <th> total chars </th>
+    <td> ? </td>
+  </tr>
+</table>
+
+
+<style lang="scss">
+
+@use './src/styles/pages/article' as *;
+
+</style>
