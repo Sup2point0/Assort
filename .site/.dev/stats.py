2fca0ab53728566f4753d591f9ddff2b6156aa93
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-11-24 22:13:31 +0000

    assort

---
+++ .site/.dev/stats.py
@@ -0,0 +1,38 @@
+'''
+Script for calculating repository statistics
+'''
+
+print("python / processing stats...")
+
+import json
+import os
+from pathlib import Path
+
+import git
+
+
+root = Path(__file__).parents[2]
+repo = git.Repo(repo_path)
+
+files = list(root.glob("**/*"))
+folders = list(root.glob("**/*"))
+
+data = {
+  "commits": repo.git.rev_list("--count", "HEAD")
+  "total-size": sum(os.stat(each).st_size for each in files),
+
+  "source-files": len(files),
+    "readme": sum(1 for each in files if each.name.casefold() == "readme.md",
+    "md": sum(1 for each in files if each.suffix == ".md"),
+    "svelte": sum(1 for each in files if each.suffix == ".svelte"),
+    "ts": sum(1 for each in files if each.suffix == ".ts"),
+    "scss": sum(1 for each in files if each.suffix == ".scss"),
+  
+  "source-folders": len(folders),
+}
+
+with open(root / ".site/src/data/stats.json", "w") as dest:
+  json.dump(data, dest, indent = 2)
+
+
+print("       / done!"
